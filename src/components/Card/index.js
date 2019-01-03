539cc43c048e300d40bcda230426deaae9276677
Dmitrii Zakharov <dzakharov@splunk.com>
2019-01-02 20:03:23 -0800

    Initial commit

---
+++ src/components/Card/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+//import CardBtn from "../CardBtn";
+import "./style.css";
+import ruidiaz from './images/Ruidiaz.jpeg';
+
+function Card(props) {
+  return (
+    <div
+      className="card"
+      style={{
+        backgroundImage: props.image ? `props.image` : "none"
+      }}
+    >
+      {/* {!props.image && <i className="fa fa-spinner fa-spin" aria-hidden="true" />} */}
+      <img src={ruidiaz} width="100" height="80"></img>
+    </div>
+  );
+}
+
+export default Card;
