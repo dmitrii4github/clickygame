10f50cfb0c97f908f5925755a7f3a70a10dcf47d
Dmitrii Zakharov <dzakharov@splunk.com>
2019-01-07 09:45:24 -0800

    Updates

---
+++ precache-manifest.e817b776ac824589291e3f3ae6ec336b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "06456f363ea72957ca4a",
+    "url": "/clickygame/static/js/main.06456f36.chunk.js"
+  },
+  {
+    "revision": "9a542b9b903995087c09",
+    "url": "/clickygame/static/js/1.9a542b9b.chunk.js"
+  },
+  {
+    "revision": "06456f363ea72957ca4a",
+    "url": "/clickygame/static/css/main.35e009b1.chunk.css"
+  },
+  {
+    "revision": "48cad476010d2dcfa31b6385b60ddc19",
+    "url": "/clickygame/index.html"
+  }
+];
